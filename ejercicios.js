3fcde6172cfdbe98284e9562b0e579d547502dbe
unknown <leonardo.balmes@tecdesoftware.edu.mx>
2025-11-06 07:42:30 -0600

    ejercicio 12 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -194,6 +194,9 @@ function tablaMultiplicar(numero) {
 function numerosPares(n) {
   // TODO: Crea un array con todos los nÃºmeros pares hasta n
   const pares = [];
+  for (let i = 2; i <= n; i += 2) {
+    pares.push(i);
+  }
   return pares;
 }
 
