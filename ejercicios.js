26ea405e4d72181defb143a3551ce1b1690739a0
unknown <leonardo.balmes@tecdesoftware.edu.mx>
2025-11-06 07:41:16 -0600

    ejercicio 11 resuelto

--- ejercicios.js
+++ ejercicios.js
@@ -177,8 +177,11 @@ function sumaHastaN(n) {
  * Ejemplo: tablaMultiplicar(3) debe retornar [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]
  */
 function tablaMultiplicar(numero) {
-  // TODO: Genera un array con la tabla de multiplicar
+  // TODO: Genera un array con la tabla de multiplicar del nÃºmero
   const tabla = [];
+  for (let i = 1; i <= 10; i++) {
+    tabla.push(numero * i);
+  }
   return tabla;
 }
 
